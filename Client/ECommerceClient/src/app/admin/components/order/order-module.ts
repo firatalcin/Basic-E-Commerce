74844d633b42b008d8860154d210dd4dc2fcd4f9
Fırat Alçın <firatalcin04@gmail.com>
2025-09-28 18:47:27 +0300

    Admin ve UI Altyapısının Geliştirilmesi

---
+++ Client/ECommerceClient/src/app/admin/components/order/order-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Order } from './order';
+
+
+
+@NgModule({
+  declarations: [
+    Order
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrderModule { }
---
+++ Client/ECommerceClient/src/app/app-module.ts
@@ -0,0 +1,26 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { AdminModule } from './admin/admin-module';
+import { UiModule } from './ui/ui-module';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AdminModule,
+    UiModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
